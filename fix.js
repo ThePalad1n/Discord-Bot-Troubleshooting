069b2994df22644f1637efb47c800a7533d912db
ThePalad1n <31965502+ThePalad1n@users.noreply.github.com>
2022-01-27 16:27:17 -0500

    Created fix.js
    should throw 0 edits now

---
+++ fix.js
@@ -0,0 +1,30 @@
+module.exports = { name: 'command', description: "Embeds!",
+
+execute(message, args, Discord){
+
+
+    const newEmbed = new Discord.MessageEmbed()
+    .setColor('#304281')
+    .setTitle('Rules')
+    .setURL('https://www.google.com')
+    .setDescription('This is an embed for the server rules')
+    .addFields(
+        {name: 'Rule 1', value: 'Do NOT talk about Neurotators.'},
+        {name: 'Rule 2', value: 'Understand Rule 1.'},
+        {name: 'Rule 3', value: 'Be respectful. See Rule 4.'},
+        {name: 'Rule 4', value: 'No slurs or hate speech. See Rule 3.'},
+        {name: 'Rule 5', value: 'Discussion should be geared toward science, whether quantum mechanical or sociological.'},
+        {name: 'Rule 6', value: 'Have fun!'},
+    )
+    .setImage('https://www.lead-dbs.org/wp-content/uploads/HarvardOxford-261x300.png')
+    .setFooter('Make sure to check out the Rules channel, mkay?')
+    channel.send(embed)
+    channel.send({
+        embed: 
+            {
+            title: 'some title',
+            description: 'some description',
+            image: {url: 'image url'}
+            }
+        })
+    }
